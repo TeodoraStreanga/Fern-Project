96c0fa10c8e4bf636ecf9f4654a87db6618d8d35
TeodoraStreanga <enderteo13@gmail.com>
2023-05-17 22:35:55 +0300

    Added about page

--- fern-client/src/Assets/assets.js
+++ fern-client/src/Assets/assets.js
@@ -1,5 +1,11 @@
+//Icons
 export const FernLogo = require('./Icons/fern_logo.png')
-
 export const Facebook = require('./Icons/facebook.png')
 export const Twitter = require('./Icons/twitter.png')
 export const Instagram = require('./Icons/instagram.png')
+//Images
+export const Green1 = require('./Images/green1.jpg')
+export const Green2 = require('./Images/green2.jpg')
+export const Green3 = require('./Images/green3.jpg')
+export const Green4 = require('./Images/green4.jpg')
+export const Green5 = require('./Images/green5.jpg')
--- fern-client/src/Pages/AboutPage.js
+++ fern-client/src/Pages/AboutPage.js
@@ -1,11 +1,39 @@
 import React from "react"
 import "../Styles/styles.js"
+import {Green1, Green2, Green3, Green4, Green5} from "../Assets/assets.js"
 
 function AboutPage() {
   return (
-    <div className="aboutPage">
+    <div className="about-page">
+      
       <div className = "header"></div>
-      This is about!
+      
+      <div className = "about-panel">
+
+        <div className = "about-block">
+          <img className = "about-image" src = {Green5} alt = ""></img>
+
+          <div className = "about-text-block">
+            <div className = "about-title">Про нас</div>
+            <div className = "about-text">
+              Fern - це куточок природи серед міста, яка надає атмосферу спокою та затишку для відвідувачів. Маленька ідея, натхнена природою яка розрослася до цілого кафе у чарівному місті Львів.
+            </div>
+            <div className = "about-text">
+              Яскраві квіти та рослини, а також найпопулярніші та насмачніші страви української кухні і не тільки - основа для магії Fern.
+            </div>
+          </div>
+          
+        </div>
+        <div className = "gallery-panel">
+          <div className = "about-title">Галерея</div>
+          <ul className = "gallery">
+            <li><img className = "gallery-image" src ={Green1} alt = ""></img></li>
+            <li><img className = "gallery-image" src ={Green2} alt = ""></img></li>
+            <li><img className = "gallery-image" src ={Green3} alt = ""></img></li>
+            <li><img className = "gallery-image" src ={Green4} alt = ""></img></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
